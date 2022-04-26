71dd1454a1cec5ac857a451339d8a036bd3aec85
Jefferson <jeffoficialfg@gmail.com>
2022-04-26 19:56:16 -0300

    primeiro push

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APP_Social',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
